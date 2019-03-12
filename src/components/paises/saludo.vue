441fbb9e51859bc84a26dba1c065fede44d550ad
cartrrz <cartrrz.inf@gmail.com>
2019-03-12 17:52:40 -0400

    saludo

---
+++ src/components/paises/saludo.vue
@@ -0,0 +1,18 @@
+<template>
+<h1 v-text="nombre"></h1>
+</template>
+
+<script>
+export default {
+    dato() {
+        return {
+            nombre: 'carlos xD'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+>
