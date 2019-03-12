59febcce523c1377edf2bb79ee01aa1508d121c8
cartrrz <cartrrz.inf@gmail.com>
2019-03-12 18:15:57 -0400

    boton

--- src/components/paises/saludo.vue
+++ src/components/paises/saludo.vue
@@ -1,18 +1,20 @@
 <template>
-<h1 v-text="nombre"></h1>
+  <div>
+    <h1 v-text="nombre"></h1>
+    <md-button class="md-raised md-primary">Primary</md-button>
+  </div>
 </template>
 
 <script>
 export default {
-    dato() {
-        return {
-            nombre: 'carlos xD'
-        }
-    }
-}
+  dato() {
+    return {
+      nombre: "carlos xD"
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
 >
