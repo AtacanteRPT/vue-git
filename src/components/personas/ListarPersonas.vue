68c7f8591615e30c20bda8f67526816254d67695
AtacanteRPT <rpaucara13@umsa.bo>
2019-03-12 17:59:32 -0400

    componente personas

---
+++ src/components/personas/ListarPersonas.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="header">Listado Persona Componente</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.header{
+  font-size: 4rem;
+}
+</style>
